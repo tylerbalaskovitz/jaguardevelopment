340685044eaefa7f77c57b095ba793b123a417bb
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-09-30 22:54:10 -0400

    Able to get the Ti4 image in the background and have it look pretty nice

--- projects/basic/quickguide/build/romassets.h
+++ projects/basic/quickguide/build/romassets.h
@@ -1,4 +1,4 @@
-extern void *SPRITE_BUG asm("SPRITE_BUG");
+extern void *TI4 asm("TI4");
 extern void *SPRITE_BUG2 asm("SPRITE_BUG2");
 extern void *explode_sam asm("explode_sam");
 extern void *explode_sam_end asm("explode_sam_end");
